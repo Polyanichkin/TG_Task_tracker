dc6078a7e7c51c7fddb3ba9b8a5c6b04af939c49
Ilya Polyanichkin <i.polyanichkin@chaika.com>
2024-03-23 22:13:08 +0300

    1st lesson - 3st task

---
+++ HomeWorks/home_work_1/task_3.py
@@ -0,0 +1,12 @@
+date1 = input("Введите дату: ")
+task1 = input("Введите название задачи: ")
+date2 = input("Введите дату: ")
+task2 = input("Введите название задачи: ")
+date3 = input("Введите дату: ")
+task3 = input("Введите название задачи: ")
+tasks = {
+    date1: task1,
+    date2: task2,
+    date3: task3
+}
+# print(tasks)
