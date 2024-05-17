e9b0009f0e921ff7d960d42c53fe1a3eacb2a7d8
Ilia Polyanichkin <ia_polyanichkin@choosedoctor.ru>
2024-05-17 20:48:05 +0300

    Fix

--- HomeWorks/home_work_4/task_1.py
+++ HomeWorks/home_work_4/task_1.py
@@ -5,10 +5,11 @@ token = credentials.BOT_TOKEN
 
 bot = telebot.TeleBot(token)
 
+my_name = "Илья"
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
-    if "Илья" in message.text:
+    if my_name in message.text:
         bot.send_message(message.chat.id, "Ба! Знакомые все лица!")
     else:
         bot.send_message(message.chat.id, message.text)
